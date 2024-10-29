f34050fb59007e465a784bbece31d8646a5bdc04
yangbo <972849883@qq.com>
2024-10-29 23:17:02 +0800

    添加了个人信息

---
+++ pages/me/me.vue
@@ -0,0 +1,31 @@
+<template>
+ <view class="me">
+    <!-- 其他内容 -->
+    <a href="https://gitee.com/yang-zihe1235/shida-news.git" target="_blank">访问杨子赫的Gitee</a>
+    <!-- 其他内容 -->
+  </view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+.me{
+	background: #9cc19a;
+	
+	.text
+	{
+		font-size: 36rpx;
+		
+		
+	}
+}
+
+</style>
