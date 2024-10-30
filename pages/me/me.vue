d00aa6b6bca59edcec99b64a8545b15a00172297
yangbo <972849883@qq.com>
2024-10-30 08:29:25 +0800

    添加自己的个人信息

--- pages/me/me.vue
+++ pages/me/me.vue
@@ -1,7 +1,8 @@
 <template>
  <view class="me">
     <!-- 其他内容 -->
-    <a href="https://gitee.com/yang-zihe1235/shida-news.git" target="_blank">访问杨子赫的Gitee</a>
+    <a href="https://gitee.com/yang-zihe1235/shida-news.git" target="_blank">第31组,访问杨子赫的Gitee</a>
+	<text>组员: 杨子赫,张伟强</text>
     <!-- 其他内容 -->
   </view>
 </template>
@@ -19,6 +20,12 @@
 <style lang="scss">
 .me{
 	background: #9cc19a;
+
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	
 	
 	.text
 	{
